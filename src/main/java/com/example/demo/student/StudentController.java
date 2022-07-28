26b04a5df81847d6d202e585dcb163a83b8763ca
ikoyski <ikoyski@ikoyski-laptop>
2022-07-29 06:56:45 +0800

    added manually created openapi.yaml

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -3,6 +3,7 @@ package com.example.demo.student;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,26 +25,31 @@ public class StudentController {
 	}
 
 	@GetMapping(path = "api/v1/students")
+	@ResponseStatus(code = HttpStatus.OK, reason = "OK")
 	public List<Student> getStudents() {
 		return studentService.getStudents();
 	}
 
 	@GetMapping(path = "api/v1/students/{studentId}")
+	@ResponseStatus(code = HttpStatus.OK, reason = "OK")
 	public Student getStudent(@PathVariable("studentId") Long studentId) {
 		return studentService.getStudent(studentId);
 	}
 
 	@PostMapping(path = "api/v1/students")
+	@ResponseStatus(code = HttpStatus.CREATED, reason = "Created")
 	public void registerNewStudent(@RequestBody StudentRequest studentRequest) {
 		studentService.addNewStudent(studentRequest);
 	}
 
 	@DeleteMapping(path = "api/v1/students/{studentId}")
+	@ResponseStatus(code = HttpStatus.OK, reason = "Deleted")
 	public void deleteStudent(@PathVariable("studentId") Long studentId) {
 		studentService.deleteStudent(studentId);
 	}
 
 	@PutMapping(path = "api/v1/students/{studentId}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT, reason = "Updated")
 	public void updateStudent(@PathVariable("studentId") Long studentId, @RequestParam(required = false) String name,
 			@RequestParam(required = false) String email) {
 		studentService.updateStudent(studentId, name, email);
